4bc5f4a0313eb0c99278d87fdfdd3ec5e82911ee
MD. Waris ahmad <118051823+Mdwaris@users.noreply.github.com>
2026-08-29 19:27:24 +0530

    Add output for CGPA calculation task
    Output of CGPA calculation for five courses with details and final CGPA.

---
+++ OUTPUT.m
@@ -0,0 +1,49 @@
+@Mdwaris ➜ /workspaces/CodeAlpha_Cpp_Internship (main) $ ls
+ LICENSE   README.md   
+ Task_1_CGPA_Calculator.cpp                   cgpa
+ 'Task_2_Login_&_Registration.cpp'            login  task2
+ Task_3_Sudoku_Solver.cpp                     sudoku
+ Task_4_Banking_System.cpp                    banking         
+ <<<<//////////   OUTPUT -->> TASK = 02   ////////>>>
+ ===== CGPA Calculator =====
+Enter number of courses: 5
+
+Enter details for Course 1:
+Course name: DBMS
+Grade point: 4
+Credit hours: 4.0
+
+Enter details for Course 2:
+Course name: DataStructure
+Grade point: 4 
+Credit hours: 5
+
+Enter details for Course 3:
+Course name: C++
+Grade point: 3
+Credit hours: 3
+
+Enter details for Course 4:
+Course name: Python
+Grade point: 4
+Credit hours: 4.0
+
+Enter details for Course 5:
+Course name: JavaScript
+Grade point: 7
+Credit hours: 9
+GPA: 4.96
+
+========== Course Details ==========
+Course: DBMS | Grade: 4 | Credits: 4
+Course: DataStructure | Grade: 4 | Credits: 5
+Course: C++ | Grade: 3 | Credits: 3
+Course: Python | Grade: 4 | Credits: 4
+Course: JavaScript | Grade: 7 | Credits: 9
+
+Final CGPA: 4.96
+
+
+
+
+<<<<<////////////// OUTPUT = TASK = 02 /////////>>>>>>>
